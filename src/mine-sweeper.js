0a2a02f2ce2654c8ddc2ab454e54825174647aad
LetoCode <pavolov.egor@gmail.com>
2022-10-14 11:11:18 +0300

    feat: solve Minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let coords;
+
+  let resultMatrix = JSON.stringify(matrix);
+  resultMatrix = JSON.parse(resultMatrix)
+
+  resultMatrix.forEach(el => {
+    el.fill(0)
+  })
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      coords = getCoords(i, j, matrix.length, matrix[i].length);
+      resultMatrix[i][j] = getResulMatrixEl(coords);
+    }
+  }
+
+  function getResulMatrixEl(coords) {
+    count = 0;
+    coords.forEach(el => {
+      let i = el[0], j = el[1];
+
+      if (matrix[i][j] === true) { count++; }
+    })
+    return count;
+  }
+
+  function getCoords(i, j, maxI, maxJ) {
+    let xy1 = [i, j + 1];
+    let xy2 = [i, j - 1];
+    let xy3 = [i + 1, j];
+    let xy4 = [i - 1, j];
+    let xy5 = [i - 1, j - 1];
+    let xy6 = [i - 1, j + 1];
+    let xy7 = [i + 1, j + 1];
+    let xy8 = [i + 1, j - 1];
+
+    let rightCoords = getRightCoords(maxI, maxJ, xy1, xy2, xy3, xy4, xy5, xy6, xy7, xy8);
+    return rightCoords;
+  }
+
+  function getRightCoords(maxI, maxJ, ...args) {
+    let coordsRight = [];
+    args.forEach(el => {
+      if ((el[0] >= 0 && el[0] < maxI) && (el[1] >= 0 && el[1] < maxJ)) {
+        coordsRight.push(el);
+      }
+    })
+    return coordsRight
+  }
+
+  return resultMatrix;
 }
 
 module.exports = {
